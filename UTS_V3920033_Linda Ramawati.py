b2a46f0665ad7f95672089d7e3abcecd524b0546
Lindaramawa <88967914+Lindaramawa@users.noreply.github.com>
2021-10-19 00:07:18 +0700

    Add files via upload

---
+++ UTS_V3920033_Linda Ramawati.py
@@ -0,0 +1,290 @@
+import sys
+from typing import KeysView
+import numpy as np
+
+
+def cipher_encryption():
+    msg = input("Enter message: ").upper()
+
+    msg = msg.replace(" ", "")
+
+    # if message length is odd number, append 0 at the end
+    len_chk = 0
+    if len(msg) % 2 != 0:
+        msg += "0"
+        len_chk = 1
+
+    # msg to matrices
+    row = 2
+    col = int(len(msg)/2)
+    msg2d = np.zeros((row, col), dtype=int)
+
+    itr1 = 0
+    itr2 = 0
+    for i in range(len(msg)):
+        if i % 2 == 0:
+            msg2d[0][itr1] = int(ord(msg[i])-65)
+            itr1 += 1
+        else:
+            msg2d[1][itr2] = int(ord(msg[i])-65)
+            itr2 += 1
+    # for
+
+    key = input("Enter 4 letter Key String: ").upper()
+    key = key.replace(" ", "")
+
+    # key to 2x2
+    key2d = np.zeros((2, 2), dtype=int)
+    itr3 = 0
+    for i in range(2):
+        for j in range(2):
+            key2d[i][j] = ord(key[itr3])-65
+            itr3 += 1
+
+    # checking validity of the key
+    # finding determinant
+    deter = key2d[0][0] * key2d[1][1] - key2d[0][1] * key2d[1][0]
+    deter = deter % 26
+
+    # finding multiplicative inverse
+    mul_inv = -1
+    for i in range(26):
+        temp_inv = deter * i
+        if temp_inv % 26 == 1:
+            mul_inv = i
+            break
+        else:
+            continue
+    # for
+
+    if mul_inv == -1:
+        print("Invalid key")
+        sys.exit()
+    # if
+
+    encryp_text = ""
+    itr_count = int(len(msg)/2)
+    if len_chk == 0:
+        for i in range(itr_count):
+            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
+            encryp_text += chr((temp1 % 26) + 65)
+            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
+            encryp_text += chr((temp2 % 26) + 65)
+        # for
+    else:
+        for i in range(itr_count-1):
+            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
+            encryp_text += chr((temp1 % 26) + 65)
+            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
+            encryp_text += chr((temp2 % 26) + 65)
+        # for
+    # if else
+
+    print("Encrypted Text: {}".format(encryp_text))
+
+
+def cipher_decryption():
+    msg = input("Enter message: ").upper()
+    msg = msg.replace(" ", "")
+
+    # if message length is odd number, append 0 at the end
+    len_chk = 0
+    if len(msg) % 2 != 0:
+        msg += "0"
+        len_chk = 1
+
+    # msg to matrices
+    row = 2
+    col = int(len(msg) / 2)
+    msg2d = np.zeros((row, col), dtype=int)
+
+    itr1 = 0
+    itr2 = 0
+    for i in range(len(msg)):
+        if i % 2 == 0:
+            msg2d[0][itr1] = int(ord(msg[i]) - 65)
+            itr1 += 1
+        else:
+            msg2d[1][itr2] = int(ord(msg[i]) - 65)
+            itr2 += 1
+    # for
+
+    key = input("Enter 4 letter Key String: ").upper()
+    key = key.replace(" ", "")
+
+    # key to 2x2
+    key2d = np.zeros((2, 2), dtype=int)
+    itr3 = 0
+    for i in range(2):
+        for j in range(2):
+            key2d[i][j] = ord(key[itr3]) - 65
+            itr3 += 1
+    # for
+
+    # finding determinant
+    deter = key2d[0][0] * key2d[1][1] - key2d[0][1] * key2d[1][0]
+    deter = deter % 26
+
+    # finding multiplicative inverse
+    mul_inv = -1
+    for i in range(26):
+        temp_inv = deter * i
+        if temp_inv % 26 == 1:
+            mul_inv = i
+            break
+        else:
+            continue
+    # for
+
+    # adjugate matrix
+    # swapping
+    key2d[0][0], key2d[1][1] = key2d[1][1], key2d[0][0]
+
+    # changing signs
+    key2d[0][1] *= -1
+    key2d[1][0] *= -1
+
+    key2d[0][1] = key2d[0][1] % 26
+    key2d[1][0] = key2d[1][0] % 26
+
+    # multiplying multiplicative inverse with adjugate matrix
+    for i in range(2):
+        for j in range(2):
+            key2d[i][j] *= mul_inv
+
+    # modulo
+    for i in range(2):
+        for j in range(2):
+            key2d[i][j] = key2d[i][j] % 26
+
+    # cipher to plain
+    decryp_text = ""
+    itr_count = int(len(msg) / 2)
+    if len_chk == 0:
+        for i in range(itr_count):
+            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
+            decryp_text += chr((temp1 % 26) + 65)
+            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
+            decryp_text += chr((temp2 % 26) + 65)
+            # for
+    else:
+        for i in range(itr_count - 1):
+            temp1 = msg2d[0][i] * key2d[0][0] + msg2d[1][i] * key2d[0][1]
+            decryp_text += chr((temp1 % 26) + 65)
+            temp2 = msg2d[0][i] * key2d[1][0] + msg2d[1][i] * key2d[1][1]
+            decryp_text += chr((temp2 % 26) + 65)
+            # for
+
+    print("Decrypted Text: {}".format(decryp_text))
+
+    # if else
+key=input("Enter key")
+key=key.replace(" ", "")
+key=key.upper()
+def matrix(x,y,initial):
+    return [[initial for i in range(x)] for j in range(y)]
+    
+result=list()
+for c in key: #storing key
+    if c not in result:
+        if c=='J':
+            result.append('I')
+        else:
+            result.append(c)
+flag=0
+for i in range(65,91): #storing other character
+    if chr(i) not in result:
+        if i==73 and chr(74) not in result:
+            result.append("I")
+            flag=1
+        elif flag==0 and i==73 or i==74:
+            pass    
+        else:
+            result.append(chr(i))
+k=0
+my_matrix=matrix(5,5,0) #initialize matrix
+for i in range(0,5): #making matrix
+    for j in range(0,5):
+        my_matrix[i][j]=result[k]
+        k+=1
+
+def locindex(c): #get location of each character
+    loc=list()
+    if c=='J':
+        c='I'
+    for i ,j in enumerate(my_matrix):
+        for k,l in enumerate(j):
+            if c==l:
+                loc.append(i)
+                loc.append(k)
+                return loc
+            
+def encrypt():  #Encryption
+    msg=str(input("ENTER MSG:"))
+    msg=msg.upper()
+    msg=msg.replace(" ", "")             
+    i=0
+    for s in range(0,len(msg)+1,2):
+        if s<len(msg)-1:
+            if msg[s]==msg[s+1]:
+                msg=msg[:s+1]+'X'+msg[s+1:]
+    if len(msg)%2!=0:
+        msg=msg[:]+'X'
+    print("CIPHER TEXT:",end=' ')
+    while i<len(msg):
+        loc=list()
+        loc=locindex(msg[i])
+        loc1=list()
+        loc1=locindex(msg[i+1])
+        if loc[1]==loc1[1]:
+            print("{}{}".format(my_matrix[(loc[0]+1)%5][loc[1]],my_matrix[(loc1[0]+1)%5][loc1[1]]),end=' ')
+        elif loc[0]==loc1[0]:
+            print("{}{}".format(my_matrix[loc[0]][(loc[1]+1)%5],my_matrix[loc1[0]][(loc1[1]+1)%5]),end=' ')  
+        else:
+            print("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]),end=' ')    
+        i=i+2        
+                 
+def decrypt():  #decryption
+    msg=str(input("ENTER CIPHER TEXT:"))
+    msg=msg.upper()
+    msg=msg.replace(" ", "")
+    print("PLAIN TEXT:",end=' ')
+    i=0
+    while i<len(msg):
+        loc=list()
+        loc=locindex(msg[i])
+        loc1=list()
+        loc1=locindex(msg[i+1])
+        if loc[1]==loc1[1]:
+            print("{}{}".format(my_matrix[(loc[0]-1)%5][loc[1]],my_matrix[(loc1[0]-1)%5][loc1[1]]),end=' ')
+        elif loc[0]==loc1[0]:
+            print("{}{}".format(my_matrix[loc[0]][(loc[1]-1)%5],my_matrix[loc1[0]][(loc1[1]-1)%5]),end=' ')  
+        else:
+            print("{}{}".format(my_matrix[loc[0]][loc1[1]],my_matrix[loc1[0]][loc[1]]),end=' ')    
+        i=i+2        
+
+
+    
+    
+
+
+def main():
+    choice = int(input("1. Encryption Hill Cipher\n2. Decryption Hill Cipher\n3. Enkripsi Playfair Cipher\n4.Deskripsi Playfair Cipher\n5.ExitChoose(1,2,3,4,5): "))
+    if choice == 1:
+        print("---Encryption    Hill---")
+        cipher_encryption()
+    elif choice == 2:
+        print("---Decryption Hill ---")
+        cipher_decryption()
+    if choice==3:
+        encrypt()
+    elif choice==4:
+        decrypt()
+    elif choice==5:
+        exit()
+    else:
+        print("Invalid Choice")
+    
+
+if __name__ == "__main__":
+    main()
